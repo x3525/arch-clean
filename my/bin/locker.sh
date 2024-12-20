074787b3335302866310ff58fb53c4251076b31c
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2024-12-20 23:16:51 +0300

    Update locker.sh

--- my/bin/locker.sh
+++ my/bin/locker.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-brightnessctl --save
-brightnessctl set 1%
 dunstctl set-paused true
-
 i3lock --nofork --pointer default --color 000000 --ignore-empty-password --show-failed-attempts --show-keyboard-layout
-
 dunstctl set-paused false
-brightnessctl --restore
