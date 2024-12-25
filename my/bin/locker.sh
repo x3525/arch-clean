55b0adf269a0bbf44c0924aaeb2b22026984c98b
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2024-12-25 16:56:22 +0300

    Update locker.sh

--- my/bin/locker.sh
+++ my/bin/locker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dunstctl set-paused true
-i3lock --nofork --pointer default --color 000000 --ignore-empty-password --show-failed-attempts --show-keyboard-layout
+i3lock --nofork --pointer default --color 301934 --ignore-empty-password --show-failed-attempts --show-keyboard-layout
 dunstctl set-paused false
