357afef0d8f23ea1a90d17e876cfaa6ab2e83019
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-01-16 15:00:19 +0300

    Change power_menu.sh

--- user/bin/power_menu.sh
+++ user/bin/power_menu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo -e "Log Out\0icon\x1fsystem-log-out"
+echo -e "Sign out\0icon\x1fsystem-log-out"
 echo -e "Restart\0icon\x1fsystem-reboot"
-echo -e "Shut Down\0icon\x1fsystem-shutdown"
+echo -e "Shut down\0icon\x1fsystem-shutdown"
 
 case $1 in
-    "Log Out")
+    "Sign out")
         i3-msg exit
         ;;
     "Restart")
         systemctl reboot
         ;;
-    "Shut Down")
+    "Shut down")
         systemctl poweroff
         ;;
 esac
