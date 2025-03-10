6dfcdad300d6f35c21490a195e3134a4eed09554
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-03-10 08:32:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$(cat /sys/firmware/efi/fw_platform_size)" != "64" ]
+then
+    echo "System is not booted in 64-bit UEFI mode!"
+    exit 1
+fi
+
 if [ $# -ne 2 ]
 then
     echo "Usage: $0 <device> <user>"
@@ -14,34 +20,32 @@ else
     else
         user=$2
 
-        echo -n "Enter password for root: "
+        echo -n "Enter password for root:"
         read -r -s password_root
-        echo -n "Enter password again: "
+        echo
+        echo -n "Enter password for root again:"
         read -r -s password_root_again
+        echo
 
-        if [ "$password_root" -ne "$password_root_again" ]
+        if [ "$password_root" != "$password_root_again" ]
         then
             echo "Passwords do not match!"
             exit 1
         else
-            echo -n "Enter password for $user: "
+            echo -n "Enter password for $user:"
             read -r -s password_user
-            echo -n "Enter password again: "
+            echo
+            echo -n "Enter password for $user again:"
             read -r -s password_user_again
+            echo
 
-            if [ "$password_user" -ne "$password_user_again" ]
+            if [ "$password_user" != "$password_user_again" ]
             then
                 echo "Passwords do not match!"
                 exit 1
             fi
         fi
     fi
-fi
-
-if [ "$(cat /sys/firmware/efi/fw_platform_size)" -ne 64 ]
-then
-    echo "System is not booted in 64-bit UEFI mode!"
-    exit 1
 fi
 
 if ! ping -q -c 1 -w 2 "$(ip route | grep "default" | cut -d " " -f 3)" >& /dev/null
@@ -54,16 +58,13 @@ fi
 wipefs -f -a "$device"*
 
 # Partiton the disks
-if ! sfdisk "$device" << EOF
+sfdisk "$device" << EOF
 label: gpt
 
 start=        2048, size=     2097152, type=U
 start=     2099200, size=    33554432, type=S
 start=    35653632,                    type=L
 EOF
-then
-    exit 1
-fi
 
 # Format the partitions
 partitions=()
@@ -91,7 +92,7 @@ swapon "$swap"
 # Install packages
 while ! pacstrap -K /mnt - < PACKAGES
 do
-    echo "Alas, Pacman failed. Tr[Y] agai[n]?"
+    echo -n "Alas, Pacman failed. Tr[Y] agai[n]?"
     read -r
     case $REPLY in
         [nN]*)
