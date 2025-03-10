d966fd104a557b5e9ff054e65f4d7c0c10d13085
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-03-10 05:52:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,70 +26,61 @@ then
     exit 1
 fi
 
+# Erase all available signatures
+wipefs -f -a "$device"*
 
-installation_pre()
-{
-    # Erase all available signatures
-    wipefs -f -a "$device"*
-
-    # Partiton the disks
-    if ! sfdisk "$device" << EOF
+# Partiton the disks
+if ! sfdisk "$device" << EOF
 label: gpt
 
 start=        2048, size=     2097152, type=U
 start=     2099200, size=    33554432, type=S
 start=    35653632,                    type=L
 EOF
-    then
-        exit 1
-    fi
+then
+    exit 1
+fi
 
-    # Format the partitions
-    partitions=()
+# Format the partitions
+partitions=()
 
-    for partition in $(sfdisk --dump "$device" | grep "start" | cut -d ":" -f 1)
-    do
-        partitions+=("$partition")
-    done
+for partition in $(sfdisk --dump "$device" | grep "start" | cut -d ":" -f 1)
+do
+    partitions+=("$partition")
+done
 
-    uefi=${partitions[0]}
-    swap=${partitions[1]}
-    ext4=${partitions[2]}
+uefi=${partitions[0]}
+swap=${partitions[1]}
+ext4=${partitions[2]}
 
-    mkfs.fat -F 32 "$uefi"
-    mkswap "$swap"
-    mkfs.ext4 "$ext4" -F
+mkfs.fat -F 32 "$uefi"
+mkswap "$swap"
+mkfs.ext4 "$ext4" -F
 
-    # Mount the file systems
-    mount "$ext4" /mnt
-    mount "$uefi" /mnt/boot/efi --mkdir=0755
+# Mount the file systems
+mount "$ext4" /mnt
+mount "$uefi" /mnt/boot/efi --mkdir=0755
 
-    # Enable the swap partition
-    swapon "$swap"
-}
+# Enable the swap partition
+swapon "$swap"
 
+# Install packages
+while ! pacstrap -q -K /mnt - < PACKAGES
+do
+    echo "Alas, Pacman failed. Tr[Y] agai[n]?"
+    read -r
+    case $REPLY in
+        [nN]*)
+            umount --recursive /mnt
+            swapoff "$swap"
+            exit 1
+            ;;
+    esac
+done
 
-installation() {
-    # Install packages
-    while ! pacstrap -q -K /mnt - < PACKAGES
-    do
-        echo "Alas, Pacman failed. Tr[Y] agai[n]?"
-        read -r
-        case $REPLY in
-            [nN]*)
-                exit 1
-                ;;
-        esac
-    done
+# Generate an fstab file
+genfstab /mnt >> /mnt/etc/fstab
 
-    # Generate an fstab file
-    genfstab /mnt >> /mnt/etc/fstab
-
-    # Change root into the new system
-    cp install_chroot.sh /mnt
-    arch-chroot /mnt "bash install_chroot.sh"
-}
-
-
-installation_pre
-installation
+# Change root into the new system
+cp install_chroot.sh /mnt
+arch-chroot /mnt "bash install_chroot.sh"
