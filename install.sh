2d50776939912ea5bee5329ed034b95fadd68341
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-03-10 04:41:35 +0300

    Rename install2.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,95 @@
 #!/bin/bash
 
-if [ "$(id -u)" -eq 0 ]
+if [ $# -ne 1 ]
 then
-    echo "You are root!"
+    echo "Usage: $0 <device>"
+    exit 1
+else
+    device=$1
+
+    if [ ! -b "$device" ]
+    then
+        echo "Device is not a block special!"
+        exit 1
+    fi
+fi
+
+if [ "$(cat /sys/firmware/efi/fw_platform_size)" -ne 64 ]
+then
+    echo "System is not booted in 64-bit UEFI mode!"
     exit 1
 fi
 
-# Verify the master keys
-sudo pacman-key --init
-sudo pacman-key --populate
+if ! ping -q -c 1 -w 2 "$(ip route | grep "default" | cut -d " " -f 3)" >& /dev/null
+then
+    echo "Network is unreachable!"
+    exit 1
+fi
+
+# Erase all available signatures
+wipefs -f -a "$device"*
+echo "wipefs: Done..."
+echo "Press enter to continue..."
+read -r
+
+# Partiton the disks
+if ! sfdisk "$device" << EOF
+label: gpt
+
+start=        2048, size=     2097152, type=U
+start=     2099200, size=    33554432, type=S
+start=    35653632,                    type=L
+EOF
+then
+    exit 1
+fi
+echo "sfdisk: Done..."
+echo "Press enter to continue..."
+read -r
+
+# Format the partitions
+partitions=()
+
+for partition in $(sfdisk --dump "$device" | grep "start" | cut -d ":" -f 1)
+do
+    partitions+=("$partition")
+done
+
+uefi=${partitions[0]}
+swap=${partitions[1]}
+ext4=${partitions[2]}
+
+mkfs.fat -F 32 "$uefi"
+echo "mkfs.fat: Done..."
+echo "Press enter to continue..."
+read -r
+mkswap "$swap"
+echo "mkswap: Done..."
+echo "Press enter to continue..."
+read -r
+mkfs.ext4 "$ext4" -F
+echo "mkfs.ext4: Done..."
+echo "Press enter to continue..."
+read -r
+
+# Mount the file systems
+mount "$ext4" /mnt
+echo "mount ext4: Done..."
+echo "Press enter to continue..."
+read -r
+mount "$uefi" /mnt/boot/efi --mkdir=0755
+echo "mount uefi: Done..."
+echo "Press enter to continue..."
+read -r
+
+# Enable the swap partition
+swapon "$swap"
+echo "swapon swap: Done..."
+echo "Press enter to continue..."
+read -r
 
 # Install packages
-while ! cat -- PACKAGES | sudo pacman -Syu --noconfirm --needed -
+while ! pacstrap -K /mnt - < PACKAGES
 do
     echo "Alas, Pacman failed. Tr[Y] agai[n]?"
     read -r
@@ -21,55 +99,16 @@ do
             ;;
     esac
 done
+echo "pacstrap: Done..."
+echo "Press enter to continue..."
+read -r
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Generate an fstab file
+genfstab /mnt >> /mnt/etc/fstab
+echo "genfstab: Done..."
+echo "Press enter to continue..."
+read -r
 
-# Download Oh My Zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-
-# Download Powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
-# Copy files !!!
-cp -r home/. ~
-sudo cp -r \\/. / --no-preserve=ownership
-
-# Change default shell
-sudo chsh "$USER" --shell "$(which zsh)"
-
-# Generate the locales
-sudo locale-gen
-
-# Build font information cache files
-fc-cache -fv
-
-# Enable the corresponding devices
-rfkill block wlan
-
-# Disable the corresponding devices
-rfkill block bluetooth
-
-# Enable and start the network synchronization service
-sudo timedatectl set-ntp true
-
-# Set the time zone
-sudo ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
-
-# Set the Hardware Clock from the System Clock
-sudo hwclock --systohc
-
-# Enable timers
-systemctl --user enable battery-notification.timer
-sudo systemctl enable fstrim.timer
-sudo systemctl enable reflector.timer
-
-# Enable services
-sudo systemctl enable lightdm.service
-sudo systemctl enable NetworkManager.service
-sudo systemctl enable systemd-timesyncd.service
-
-# Disable services
-sudo systemctl disable autorandr-lid-listener.service
-sudo systemctl disable autorandr.service
+# Change root into the new system
+cp install_chroot.sh /mnt
+arch-chroot /mnt "bash install_chroot.sh"
