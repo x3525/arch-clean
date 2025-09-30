3864cadabe5ed32d660f68c1f47524d135a6bc96
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2025-09-30 15:28:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,14 +34,14 @@ fi
 
 if [ $# -ne 2 ]
 then
-    echo "Usage: ${0} BLOCK LOGIN"
+    printf "Usage: %s \e[4m%s\e[0m \e[4m%s\e[0m\n" "${0}" "BLOCK" "LOGIN"
     exit 1
 fi
 
-BLOCK=${1}
-LOGIN=${2}
+block=${1}
+login=${2}
 
-if [ ! -b "${BLOCK}" ]
+if [ ! -b "${block}" ]
 then
     echo "Device is not a block special!"
     exit 1
@@ -49,16 +49,16 @@ fi
 
 LC_CTYPE=C
 
-if [[ ! ${LOGIN} =~ ^[a-z][a-z0-9_][a-z0-9_]{,30}$ ]]
+if [[ ! ${login} =~ ^[a-z][a-z0-9_][a-z0-9_]{,30}$ ]]
 then
     echo "Login entry is invalid!"
     exit 1
 fi
 
-P=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "password (user)")
-R=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "password (root)")
+user=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "password (user)")
+root=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "password (root)")
 
-if [ -z "${P}" ] || [ -z "${R}" ]
+if [ -z "${user}" ] || [ -z "${root}" ]
 then
     echo "Empty passwords are not allowed!"
     exit 1
@@ -78,9 +78,9 @@ case "$(lspci -d ::03xx)" in
         packages+=(libva-intel-driver)
         ;;&
     *[nN][vV][iI][dD][iI][aA]*)
-        echo "[1] NVIDIA kernel modules - module sources"
-        echo "[2] NVIDIA open kernel modules - module sources"
-        echo "[3] Open Source 3D acceleration driver for nVidia cards"
+        echo "[1] NVIDIA kernel modules - module sources (nvidia-dkms ...)"
+        echo "[2] NVIDIA open kernel modules - module sources (nvidia-open-dkms ...)"
+        echo "[3] Open Source 3D acceleration driver for nVidia cards (xf86-video-nouveau ...)"
         echo "[q]"
 
         while true
@@ -143,10 +143,11 @@ zzzz reflector.service
 # Wait for Arch Linux keyring synchronization to complete
 zzzz archlinux-keyring-wkd-sync.timer archlinux-keyring-wkd-sync.service
 
-set -x
-set -e
+set -o xtrace
+set -o errexit
+set -o pipefail
 
-sfdisk -w always -W always "${BLOCK}" << EOF
+sfdisk -w always -W always "${block}" << EOF
 label: gpt
 unit: sectors
 
@@ -155,11 +156,11 @@ start=     2099200, size=    33554432, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
 start=    35653632,                    type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
 
-DUMP=$(sfdisk "${BLOCK}" -d)
-
-U=$(awk '/C12A7328-F81F-11D2-BA4B-00A0C93EC93B/ {print $1}' <<< "${DUMP}")
-S=$(awk '/0657FD6D-A4AB-43C4-84E5-0933C84B4F4F/ {print $1}' <<< "${DUMP}")
-L=$(awk '/0FC63DAF-8483-4772-8E79-3D69D8477DE4/ {print $1}' <<< "${DUMP}")
+read -r U S L < <(awk '
+/C12A7328-F81F-11D2-BA4B-00A0C93EC93B/ {print $1}
+/0657FD6D-A4AB-43C4-84E5-0933C84B4F4F/ {print $1}
+/0FC63DAF-8483-4772-8E79-3D69D8477DE4/ {print $1}
+' <<< "$(sfdisk "${block}" -d)" | paste -s)
 
 mkfs.vfat "${U}" -F 32
 mkfs.ext4 "${L}" -F
@@ -189,13 +190,13 @@ cp -r -- */ /mnt
 genfstab -U /mnt > /mnt/etc/fstab
 
 # Create a new user
-useradd -R /mnt -s /usr/bin/zsh -G wheel -m "${LOGIN}"
+useradd -R /mnt -s /usr/bin/zsh -G wheel -m "${login}"
 
 # Change password (user)
-echo "${P}" | passwd -R /mnt -s "${LOGIN}"
+echo "${user}" | passwd -R /mnt -s "${login}"
 
 # Change password (root)
-echo "${R}" | passwd -R /mnt -s
+echo "${root}" | passwd -R /mnt -s
 
 # Generate the locales
 arch-chroot /mnt locale-gen
