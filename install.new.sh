5a1bc3261c0cf40c450f17b273ed14b62f5bd610
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-05-31 03:08:29 +0300

    testing something

---
+++ install.new.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+
+function online() {
+    ping ping.archlinux.org -c 1 -w 1 > /dev/null 2>&1 || echo "No internet connection!"
+}
+
+function linger() {
+    for unit
+    do
+        echo "Currently waiting for $unit to complete..."
+
+        case $unit in
+            *.timer)
+                while [ -z "$(systemctl show -P ActiveEnterTimestamp "$unit")" ]
+                do
+                    online
+                    sleep 1
+                done
+                ;;
+            *.service)
+                while [ "$(systemctl is-active "$unit")" != "inactive" ]
+                do
+                    online
+                    sleep 1
+                done
+                ;;
+        esac
+    done
+}
+
+if [ ! -e /sys/firmware/efi/fw_platform_size ]
+then
+    echo "System is not booted in UEFI mode!"
+    exit 1
+fi
+
+if [ ! -f PACKAGES ]
+then
+    echo "PACKAGES file not found!"
+    exit 1
+else
+    mapfile -t packages < PACKAGES
+fi
+
+if [ $# -ne 3 ]
+then
+    printf "Usage: %s \e[4m%s\e[0m \e[4m%s\e[0m %s|%s\n" "$0" "BLOCK" "LOGIN" "1" "2"
+    exit 1
+fi
+
+dvc=$1
+lgn=$2
+
+if [ ! -b "$dvc" ]
+then
+    echo "Device is not a block special!"
+    exit 1
+fi
+
+case $3 in
+    2)
+        IFS=: read -r f l _ < <(parted -m $dvc unit s print free | grep free | cut -d : -f 2-4 | tr -d s | sort -t : -k 3,3nr | head -n 1)
+
+        sectors=$((16 * 1024 ** 3 / $(</sys/block/${dvc##*/}/queue/logical_block_size)))
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
+LC_CTYPE=C
+
+if [[ ! $lgn =~ ^[a-z][a-z0-9_][a-z0-9_]{,30}$ ]]
+then
+    echo "Login entry is invalid!"
+    exit 1
+fi
+
+user1=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (user)")
+user2=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (user)")
+root1=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (root)")
+root2=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (root)")
+
+if [ -z "$user1" ] || [ -z "$user2" ] || [ "$user1" != "$user2" ] || [ -z "$root1" ] || [ -z "$root2" ] || [ "$root1" != "$root2" ]
+then
+    echo "Passwords do not match!"
+    exit 1
+fi
+
+case "$(lspci -d ::03xx)" in
+    *[aA][mM][dD]*)
+        packages+=(mesa)
+        packages+=(vulkan-radeon)
+        packages+=(xf86-video-ati)
+        packages+=(xf86-video-amdgpu)
+        ;;&
+    *[iI][nN][tT][eE][lL]*)
+        packages+=(mesa)
+        packages+=(vulkan-intel)
+        packages+=(intel-gpu-tools)
+        packages+=(intel-media-driver)
+        packages+=(libva-intel-driver)
+        ;;&
+    *[nN][vV][iI][dD][iI][aA]*)
+        select package in nvidia-open-dkms xf86-video-nouveau
+        do
+            case $REPLY in
+                1)
+                    packages+=("$package")
+                    packages+=(dkms)
+                    packages+=(libva-nvidia-driver)
+                    break
+                    ;;
+                2)
+                    packages+=("$package")
+                    packages+=(mesa)
+                    packages+=(vulkan-nouveau)
+                    break
+                    ;;
+                *)
+                    exit 0
+                    ;;
+            esac
+        done
+        ;;
+esac
+
+case "$(grep vendor_id /proc/cpuinfo)" in
+    *[aA][mM][dD]*)
+        packages+=(amd-ucode)
+        ;;
+    *[iI][nN][tT][eE][lL]*)
+        packages+=(intel-ucode)
+        ;;
+esac
+
+if grep -q snd_sof /proc/modules
+then
+    packages+=(sof-firmware)
+fi
+
+echo "Starting sanity checks..."
+
+while [ "$(timedatectl show -P NTPSynchronized)" != "yes" ]
+do
+    online
+    sleep 1
+done
+
+linger reflector.service archlinux-keyring-wkd-sync.timer archlinux-keyring-wkd-sync.service
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+
+case $3 in
+    1)
+        sfdisk -w always -W always $dvc << EOF
+label: gpt
+unit: sectors
+
+start=    2048,size= 2097152,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+start= 2099200,size=33554432,type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+start=35653632,              type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+EOF
+
+read -r U S L < <(awk '
+/C12A7328-F81F-11D2-BA4B-00A0C93EC93B/ {print $1}
+/0657FD6D-A4AB-43C4-84E5-0933C84B4F4F/ {print $1}
+/0FC63DAF-8483-4772-8E79-3D69D8477DE4/ {print $1}
+' <<< "$(sfdisk "$dvc" -d)" | paste -s)
+;;
+    2)
+        before=$(lsblk -nrpo NAME $dvc)
+
+        sfdisk --append $dvc << EOF
+label: gpt
+unit: sectors
+
+start=              $f,size=                $sectors,type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+start=$((f + sectors)),size=$((l - f - sectors + 1)),type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+EOF
+
+read -r U < <(awk '
+/C12A7328-F81F-11D2-BA4B-00A0C93EC93B/ {print $1}
+' <<< "$(sfdisk "$dvc" -d)" | paste -s)
+
+        after=$(lsblk -nrpo NAME $dvc)
+
+        read -r S L < <(comm -1 -3 <(echo $before | sort) <(echo $after | sort) | paste -s)
+        ;;
+esac
+
+partprobe $dvc
+udevadm settle
+
+case $3 in
+    1)
+        mkfs.vfat "$U" -F 32
+        ;;
+esac
+
+mkfs.vfat "$U" -F 32
+mkfs.ext4 "$L" -F
+
+mount -m "$L" /mnt
+mount -m "$U" /mnt/efi
+
+mkswap "$S"
+swapon "$S"
+
+while ! pacstrap -K /mnt base base-devel linux linux-firmware linux-headers os-prober "${packages[@]}"
+do
+    echo -n "Alas, Pacman failed. Try agai[n]? "
+
+    read -r
+
+    case $REPLY in
+        n|N)
+            exit 1
+            ;;
+    esac
+done
+
+cp -r -- */ /mnt
+
+# Generate fstab file
+genfstab -U /mnt > /mnt/etc/fstab
+
+# Create a new user
+useradd -R /mnt -s /usr/bin/zsh -m -G wheel "$lgn"
+
+# Change password (user)
+echo "$user1" | passwd -s -R /mnt "$lgn"
+
+# Change password (root)
+echo "$root1" | passwd -s -R /mnt
+
+# Generate the locales
+arch-chroot /mnt locale-gen
+
+# Set the Hardware Clock from the System Clock
+arch-chroot /mnt hwclock -w
+
+# Enable timers
+arch-chroot /mnt systemctl enable fstrim.timer
+arch-chroot /mnt systemctl enable reflector.timer
+
+# Enable services
+arch-chroot /mnt systemctl enable getty@tty1.service
+arch-chroot /mnt systemctl enable NetworkManager.service
+arch-chroot /mnt systemctl enable systemd-timesyncd.service
+
+# Install GRUB to a device
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB --removable
+
+# Generate a GRUB configuration file
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
