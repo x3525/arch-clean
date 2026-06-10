ee0651f2b731226e389b89cee656289609300891
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-10 10:16:18 +0300

    Update power.sh

--- etc/skel/.config/rofi/scripts/power.sh
+++ etc/skel/.config/rofi/scripts/power.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -en "poweroff\0permanent\x1ftrue\x1ficon\x1fsystem-shut-down\x1fdisplay\x1fShut Down\x1finfo\x1fsystemctl\n"
-echo -en "reboot\0permanent\x1ftrue\x1ficon\x1fsystem-restart\x1fdisplay\x1fRestart\x1finfo\x1fsystemctl\n"
-echo -en "exit\0permanent\x1ftrue\x1ficon\x1fsystem-log-out\x1fdisplay\x1fLog Out\x1finfo\x1fi3-msg\n"
+echo -en "systemctl poweroff\0permanent\x1ftrue\x1ficon\x1fsystem-shut-down\x1fdisplay\x1fShut Down\n"
+echo -en "systemctl reboot\0permanent\x1ftrue\x1ficon\x1fsystem-restart\x1fdisplay\x1fRestart\n"
+echo -en "i3-msg exit\0permanent\x1ftrue\x1ficon\x1fsystem-log-out\x1fdisplay\x1fLog Out\n"
 
-[ -z "$ROFI_INFO" ] || coproc ( "$ROFI_INFO" "$1" &> /dev/null )
+[ -z "$1" ] || coproc ( $1 &> /dev/null )
