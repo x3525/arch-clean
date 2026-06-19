e1e8eb945768f184d5ae3df809a68eb2de2de8ad
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-19 15:42:22 +0300

    Update power.sh

--- etc/skel/.config/rofi/scripts/power.sh
+++ etc/skel/.config/rofi/scripts/power.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+if [ -n "$1" ]
+then
+    coproc { systemctl "$1" &> /dev/null; }
+    exit 0
+fi
+
 echo -e "poweroff\0icon\x1fsystem-shut-down\x1fpermanent\x1ftrue"
 echo -e "reboot\0icon\x1fsystem-restart\x1fpermanent\x1ftrue"
-
-[ -z "$1" ] || coproc { systemctl "$1" &> /dev/null; }
