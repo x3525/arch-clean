19058008a4fcf39052abe22aa4249614ea243289
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-20 00:29:57 +0300

    Update 40-bindkey.zsh

--- .dotfiles/.config/zsh/40-bindkey.zsh
+++ .dotfiles/.config/zsh/40-bindkey.zsh
@@ -1,14 +1,20 @@
 # Select keymap 'emacs' for any operations by the current command.
 bindkey -e
 
+autoload -Uz up-line-or-beginning-search
+zle -N up-line-or-beginning-search
+# Use the whole line prefix up to the cursor position for searching backwards.
+bindkey '^[[A' up-line-or-beginning-search
+
+autoload -Uz down-line-or-beginning-search
+zle -N down-line-or-beginning-search
+# Use the whole line prefix up to the cursor position for searching forwards.
+bindkey '^[[B' down-line-or-beginning-search
+
 # Move to the previous event in the history list.
 bindkey '^[[5~' up-line-or-history
 # Move to the next event in the history list.
 bindkey '^[[6~' down-line-or-history
-# Move up within the buffer.
-bindkey '^[[A' up-line-or-beginning-search
-# Move down within the buffer.
-bindkey '^[[B' down-line-or-beginning-search
 # Delete the character under the cursor.
 bindkey '^[[3~' delete-char
 # Move to the previous completion rather than the next.
