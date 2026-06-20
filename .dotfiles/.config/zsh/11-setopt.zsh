bf4d5eddf478b97b01ccae8a31abef2d21d8cf00
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-20 19:19:14 +0300

    Update and rename 10-setopt.zsh to 11-setopt.zsh

--- .dotfiles/.config/zsh/11-setopt.zsh
+++ .dotfiles/.config/zsh/11-setopt.zsh
@@ -1,13 +1,3 @@
-# When the last character resulting from a completion is a slash and the next character typed is a slash, remove the slash.
-unsetopt AUTO_REMOVE_SLASH
-# Perform textual history expansion, csh-style, treating the character ! specially.
-unsetopt BANG_HIST
-# If this option is unset, output flow control via start/stop characters (usually assigned to ^S/^Q) is disabled in the shell's editor.
-unsetopt FLOW_CONTROL
-# Allow comments even in interactive shells.
-unsetopt INTERACTIVE_COMMENTS
-# Allow the character sequence '' to signify a single quote within singly quoted strings.
-unsetopt RC_QUOTES
 # If a completion is performed with the cursor within a word, and a full completion is inserted, the cursor is moved to the end of the word.
 setopt ALWAYS_TO_END
 # Make cd push the old directory onto the directory stack.
