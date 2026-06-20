79df3e8bc1f437bff2e1b07f3e169b4a70cc5200
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-20 20:01:34 +0300

    Update 51-bindkey.zsh

--- .dotfiles/.config/zsh/51-bindkey.zsh
+++ .dotfiles/.config/zsh/51-bindkey.zsh
@@ -3,8 +3,6 @@ bindkey -e
 
 # Delete the character under the cursor.
 bindkey '^[[3~' delete-char
-# Move to the previous completion rather than the next.
-bindkey '^[[Z' reverse-menu-complete
 # Move to the previous event in the history list.
 bindkey '^[[5~' up-line-or-history
 # Move to the next event in the history list.
@@ -13,6 +11,8 @@ bindkey '^[[6~' down-line-or-history
 bindkey '^[[A' up-line-or-beginning-search
 # Move down within the buffer, otherwise search for a history line matching the start of the current line.
 bindkey '^[[B' down-line-or-beginning-search
+# Move to the previous completion rather than the next.
+bindkey '^[[Z' reverse-menu-complete
 
 case $TERM in
     linux)
@@ -22,15 +22,15 @@ case $TERM in
         bindkey '^[[4~' end-of-line
         ;;
     *)
-        # Move to the beginning of the line.
-        bindkey '^[[H' beginning-of-line
-        # Move to the end of the line.
-        bindkey '^[[F' end-of-line
-        # Move to the beginning of the previous word.
-        bindkey '^[[1;5D' backward-word
         # Move to the beginning of the next word.
         bindkey '^[[1;5C' forward-word
+        # Move to the beginning of the previous word.
+        bindkey '^[[1;5D' backward-word
         # Kill the current word.
         bindkey '^[[3;5~' kill-word
+        # Move to the end of the line.
+        bindkey '^[[F' end-of-line
+        # Move to the beginning of the line.
+        bindkey '^[[H' beginning-of-line
         ;;
 esac
