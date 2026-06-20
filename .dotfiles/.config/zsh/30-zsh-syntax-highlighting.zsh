b48b7cc5277a7f4ea3ca532900385717084b6d06
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-20 15:27:35 +0300

    Update 30-zsh-syntax-highlighting.zsh

--- .dotfiles/.config/zsh/30-zsh-syntax-highlighting.zsh
+++ .dotfiles/.config/zsh/30-zsh-syntax-highlighting.zsh
@@ -2,23 +2,23 @@ typeset -gA ZSH_HIGHLIGHT_STYLES
 
 ZSH_HIGHLIGHT_STYLES=(
     # unknown tokens / errors
-    [unknown-token]=fg=red
+    [unknown-token]=fg=#dc322f
     # shell reserved words
-    [reserved-word]=fg=yellow
+    [reserved-word]=fg=#b58900
     # aliases
-    [alias]=fg=green
+    [alias]=fg=#859900
     # suffix aliases
     [suffix-alias]=none
     # global aliases
     [global-alias]=none
     # shell builtin commands
-    [builtin]=fg=green
+    [builtin]=fg=#859900
     # function names
-    [function]=fg=green
+    [function]=fg=#859900
     # command names
-    [command]=fg=green
+    [command]=fg=#859900
     # precommand modifiers
-    [precommand]=fg=green
+    [precommand]=fg=#859900
     # command separation tokens
     [commandseparator]=none
     # hashed commands
@@ -34,9 +34,9 @@ ZSH_HIGHLIGHT_STYLES=(
     # path separators in prefixes of existing filenames
     [path_prefix_pathseparator]=underline
     # globbing expressions
-    [globbing]=fg=blue
+    [globbing]=fg=#268bd2
     # history expansion expressions
-    [history-expansion]=fg=blue
+    [history-expansion]=fg=#268bd2
     # command substitutions
     [command-substitution]=none
     # an unquoted command substitution
@@ -44,17 +44,17 @@ ZSH_HIGHLIGHT_STYLES=(
     # a quoted command substitution
     [command-substitution-quoted]=none
     # command substitution delimiters
-    [command-substitution-delimiter]=fg=magenta
+    [command-substitution-delimiter]=fg=#d33682
     # an unquoted command substitution delimiters
-    [command-substitution-delimiter-unquoted]=fg=magenta
+    [command-substitution-delimiter-unquoted]=fg=#d33682
     # a quoted command substitution delimiters
-    [command-substitution-delimiter-quoted]=fg=magenta
+    [command-substitution-delimiter-quoted]=fg=#d33682
     # process substitutions
     [process-substitution]=none
     # process substitution delimiters
-    [process-substitution-delimiter]=fg=magenta
+    [process-substitution-delimiter]=fg=#d33682
     # arithmetic expansion
-    [arithmetic-expansion]=fg=magenta
+    [arithmetic-expansion]=fg=#d33682
     # single-hyphen options
     [single-hyphen-option]=none
     # double-hyphen options
@@ -64,31 +64,31 @@ ZSH_HIGHLIGHT_STYLES=(
     # unclosed backtick command substitution
     [back-quoted-argument-unclosed]=none
     # backtick command substitution delimiters
-    [back-quoted-argument-delimiter]=fg=magenta
+    [back-quoted-argument-delimiter]=fg=#d33682
     # single-quoted arguments
-    [single-quoted-argument]=fg=yellow
+    [single-quoted-argument]=fg=#b58900
     # unclosed single-quoted arguments
-    [single-quoted-argument-unclosed]=fg=yellow
+    [single-quoted-argument-unclosed]=fg=#b58900
     # double-quoted arguments
-    [double-quoted-argument]=fg=yellow
+    [double-quoted-argument]=fg=#b58900
     # unclosed double-quoted arguments
-    [double-quoted-argument-unclosed]=fg=yellow
+    [double-quoted-argument-unclosed]=fg=#b58900
     # dollar-quoted arguments
-    [dollar-quoted-argument]=fg=yellow
+    [dollar-quoted-argument]=fg=#b58900
     # unclosed dollar-quoted arguments
-    [dollar-quoted-argument-unclosed]=fg=yellow
+    [dollar-quoted-argument-unclosed]=fg=#b58900
     # two single quotes inside single quotes when the RC_QUOTES option is set
     [rc-quote]=none
     # parameter expansion inside double quotes
-    [dollar-double-quoted-argument]=fg=cyan
+    [dollar-double-quoted-argument]=fg=#2aa198
     # backslash escape sequences inside double-quoted arguments
-    [back-double-quoted-argument]=fg=cyan
+    [back-double-quoted-argument]=fg=#2aa198
     # backslash escape sequences inside dollar-quoted arguments
-    [back-dollar-quoted-argument]=fg=cyan
+    [back-dollar-quoted-argument]=fg=#2aa198
     # parameter assignments
     [assign]=none
     # redirection operators
-    [redirection]=fg=yellow
+    [redirection]=fg=#b58900
     # elided parameters in command position
     [comment]=none
     # named file descriptor
@@ -96,7 +96,7 @@ ZSH_HIGHLIGHT_STYLES=(
     # numeric file descriptor
     [numeric-fd]=none
     # a command word other than one of those enumerated above
-    [arg0]=fg=green
+    [arg0]=fg=#859900
     # everything else
     [default]=none
 )
