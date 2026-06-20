3777d8a3c0bea2f7d8f1f94d37718620fe119d77
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-20 17:50:53 +0300

    Update 60-alias.zsh

--- .dotfiles/.config/zsh/60-alias.zsh
+++ .dotfiles/.config/zsh/60-alias.zsh
@@ -4,8 +4,8 @@ alias grep='grep --color=auto --exclude-dir={.git,.venv,venv}'
 alias ls='ls --color=auto'
 alias rm='trash-put --verbose'
 alias test-speaker='speaker-test --channels=2 --test=wav --nloops=1'
-alias xc='xsel --clipboard --input'
-alias xp='xsel --clipboard --output'
+alias xi='xsel --clipboard --input'
+alias xo='xsel --clipboard --output'
 
 for i ({3..9})
     alias -g ${(l:i::.:)}=${(l:$(((i-1)*3))::../:)}
