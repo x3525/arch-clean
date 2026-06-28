b6065a0d40b41c6e8c8bfe3fd3f66b9ad05aa672
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-28 15:34:07 +0300

    Update 50-completion.zsh

--- .dotfiles/.config/zsh/50-completion.zsh
+++ .dotfiles/.config/zsh/50-completion.zsh
@@ -1,10 +1,17 @@
 compinit
 
-zstyle ':completion:*' completer _extensions _complete
+zstyle ':completion:*' completer \
+    _extensions _complete
 
-zstyle ':completion:*' complete-options true
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' menu select
-zstyle ':completion:*' special-dirs true
-zstyle ':completion:*' squeeze-slashes true
+zstyle ':completion:*' complete-options \
+    true
+zstyle ':completion:*' list-colors \
+    ${(s.:.)LS_COLORS}
+zstyle ':completion:*' matcher-list \
+    'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' menu \
+    select
+zstyle ':completion:*' special-dirs \
+    true
+zstyle ':completion:*' squeeze-slashes \
+    true
