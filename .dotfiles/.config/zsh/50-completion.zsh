9092bfe259b382594b212dcf8800aba350b42afa
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-28 12:54:01 +0300

    Update 50-completion.zsh

--- .dotfiles/.config/zsh/50-completion.zsh
+++ .dotfiles/.config/zsh/50-completion.zsh
@@ -1,18 +1,10 @@
-# The initialization script redefines all the widgets which perform completion.
 compinit
 
-# The strings given as the value of this style provide the names of the completer functions to use.
 zstyle ':completion:*' completer _extensions _complete
 
-# This is used by the completer for cd, chdir and pushd.
 zstyle ':completion:*' complete-options true
-# If the zsh/complist module is loaded, this style can be used to set color specifications.
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-# This style can be set to a list of match specifications that are to be applied everywhere.
 zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
-# If the value contains the string 'select', menu selection will be started unconditionally.
 zstyle ':completion:*' menu select
-# If this style is set to 'true', it will add both '.' and '..' as possible completions.
 zstyle ':completion:*' special-dirs true
-# If set to 'true', sequences of slashes in filename paths will be treated as a single slash.
 zstyle ':completion:*' squeeze-slashes true
