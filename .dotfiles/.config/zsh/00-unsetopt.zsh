f11c1ae5bf1c5c4760bf06b6d0d1266c06d00a33
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-06-30 14:36:26 +0300

    Update 00-unsetopt.zsh

--- .dotfiles/.config/zsh/00-unsetopt.zsh
+++ .dotfiles/.config/zsh/00-unsetopt.zsh
@@ -2,11 +2,11 @@
 unsetopt AUTO_CD
 # When the last character resulting from a completion is a slash and the next character typed is a slash, remove the slash.
 unsetopt AUTO_REMOVE_SLASH
-# Perform textual history expansion, csh-style, treating the character ! specially.
+# Perform textual history expansion, csh-style, treating the character "!" specially.
 unsetopt BANG_HIST
 # If this option is unset, output flow control via start/stop characters (usually assigned to ^S/^Q) is disabled in the shell's editor.
 unsetopt FLOW_CONTROL
 # Allow comments even in interactive shells.
 unsetopt INTERACTIVE_COMMENTS
-# Allow the character sequence '' to signify a single quote within singly quoted strings.
+# Allow the character sequence "''" to signify a single quote within singly quoted strings.
 unsetopt RC_QUOTES
