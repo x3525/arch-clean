3ed1ca2c25a91ef9e05876674bdfa54d9bf36251
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-07-01 20:42:20 +0300

    Create 30-zle_highlight.zsh

---
+++ .dotfiles/.config/zsh/30-zle_highlight.zsh
@@ -0,0 +1,14 @@
+zle_highlight=(
+    # Any text within the command line not affected by any other highlighting.
+    default:none
+    # The area of the command line matched by the search string or pattern.
+    isearch:standout
+    # The currently selected text.
+    region:none
+    # Individual characters that have no direct printable representation.
+    special:none
+    # Suffixes that will be removed if the completion ends at that point.
+    suffix:none
+    # The characters that were inserted.
+    paste:none
+)
