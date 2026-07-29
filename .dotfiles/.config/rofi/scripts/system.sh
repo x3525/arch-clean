ea89fbd43c7e672bd028d7ec8798016567ea088a
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-07-29 16:19:30 +0300

    Update system.sh

--- .dotfiles/.config/rofi/scripts/system.sh
+++ .dotfiles/.config/rofi/scripts/system.sh
@@ -8,9 +8,9 @@ then
     exit
 fi
 
-while read -r command icon
+while read -r string icon
 do
-    echo -e "$command\0icon\x1f$icon\x1fpermanent\x1ftrue"
+    echo -e "$string\0icon\x1f$icon\x1fpermanent\x1ftrue"
 done << EOF
 poweroff system-shut-down
 reboot system-restart
