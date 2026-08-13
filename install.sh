4679036b498b18c5a2ef2830fba9e110a8a45de7
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-08-13 04:03:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -88,8 +88,8 @@ else
     username=$1
 fi
 
-root=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (root) [press tab for no echo]")
-user=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (user) [press tab for no echo]")
+root=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (root)")
+user=$(systemd-ask-password --timeout=0 --echo=yes --emoji=no "Enter a password (user)")
 
 if [ -z "$root" ] || [ -z "$user" ]
 then
