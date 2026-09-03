b9957269dc918f9b457c75aa276ff1468be65dd9
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-09-03 18:28:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -195,20 +195,6 @@ then
     packages+=(sof-firmware)
 fi
 
-if [ ! -d /mnt/etc ]
-then
-    mkdir /mnt/etc
-fi
-
-cp etc/vconsole.conf /mnt/etc/vconsole.conf
-
-if [ ! -d /mnt/etc/modprobe.d ]
-then
-    mkdir /mnt/etc/modprobe.d
-fi
-
-cp etc/modprobe.d/* /mnt/etc/modprobe.d
-
 while ! pacstrap -K /mnt base base-devel linux linux-firmware linux-headers "${packages[@]}"
 do
     read -r -p "Alas, Pacman failed. Try agai[n]? "
