d54fc470273b5aa716b4cdc64975df115817350b
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-09-06 04:11:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,8 +16,6 @@ atexit () {
 
 trap atexit EXIT
 
-export SYSTEMD_PAGER=
-
 linger () {
     for unit
     do
@@ -25,7 +23,7 @@ linger () {
 
         case $unit in
             *.timer)
-                while [ -z "$(systemctl -P ActiveEnterTimestamp show "$unit")" ]
+                while [ -z "$(systemctl --no-pager --value --property=ActiveEnterTimestamp show "$unit")" ]
                 do
                     sleep 1
                 done
@@ -33,7 +31,7 @@ linger () {
             *.service)
                 while true
                 do
-                    case $(systemctl -P SubState show "$unit") in
+                    case $(systemctl --no-pager --value --property=SubState show "$unit") in
                         dead)
                             break
                             ;;
@@ -110,7 +108,7 @@ fi
 
 echo "Starting sanity checks..."
 
-while [ "$(timedatectl -P NTPSynchronized show)" != "yes" ]
+while [ "$(timedatectl --no-pager --value --property=NTPSynchronized show)" != "yes" ]
 do
     sleep 1
 done
