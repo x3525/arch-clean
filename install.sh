1bf1a95d64cfe867a7bc4d187d64beda53b605ad
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-09-07 19:32:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -188,7 +188,7 @@ esac
 case $(cat /proc/modules) in
     *snd_sof\ *)
         packages+=(sof-firmware)
-        ;;
+        ;;&
     *snd_asihpi\ *|*snd_cs46xx\ *|*snd_darla2[04]\ *|*snd_echo3g\ *|*snd_emu10k1\ *|*snd_gina2[04]\ *|*snd_hda_codec_ca0132\ *|*snd_hdsp\ *|*snd_indigo\ *|*snd_indigodj\ *|*snd_indigodjx\ *|*snd_indigoio\ *|*snd_indigoiox\ *|*snd_layla2[04]\ *|*snd_mia\ *|*snd_mixart\ *|*snd_mona\ *|*snd_pcxhr\ *|*snd_vx_lib\ *)
         packages+=(alsa-firmware)
         ;;
