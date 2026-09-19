63022294104f2c10b5f9667fa33d2e367c46123b
Serhat Çelik <71452635+x3525@users.noreply.github.com>
2026-09-19 17:49:08 +0300

    Update 30-zstyle.zsh

--- .dotfiles/.zsh/30-zstyle.zsh
+++ .dotfiles/.zsh/30-zstyle.zsh
@@ -5,18 +5,23 @@ else
     compinit -C
 fi
 
-zstyle ':completion:*' completer \
-    _extensions _complete
+# The strings given as the value of this style provide the names of the completer functions to use.
+zstyle ':completion:*' completer _extensions _complete
 
-zstyle ':completion:*' complete-options \
-    true
-zstyle ':completion:*' list-colors \
-    ${(s.:.)LS_COLORS}
-zstyle ':completion:*' matcher-list \
-    'r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' menu \
-    select
-zstyle ':completion:*' special-dirs \
-    true
-zstyle ':completion:*' squeeze-slashes \
-    true
+# If it is "true", options will be completed after an initial - unless there is a preceding -- on the command line.
+zstyle ':completion:*' complete-options true
+
+# If the zsh/complist module is loaded, this style can be used to set color specifications.
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# This style can be set to a list of match specifications that are to be applied everywhere.
+zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
+
+# If the value contains the string "select", menu selection will be started unconditionally.
+zstyle ':completion:*' menu select
+
+# If this style is set to "true", it will add both "." and ".." as possible completions.
+zstyle ':completion:*' special-dirs true
+
+# If set to "true", sequences of slashes in filename paths will be treated as a single slash.
+zstyle ':completion:*' squeeze-slashes true
